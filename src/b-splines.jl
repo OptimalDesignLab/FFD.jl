651e8d865d59fd3e558415b38f80d4fbfd885f37
Kinshuk Panda <pandak@rpi.edu>
2016-05-24 15:18:10 -0400

    implementation of basisFunctions from b-splines2.jl seems to work

--- src/b-splines.jl
+++ src/b-splines.jl
@@ -19,7 +19,44 @@ Computes the basis functions needed to compute B-Splines
 SOURCE: "A practical guide to splines" bt C. de Boor Page 112 BSPLVB
 
 """->
+function basisFunctions(T, jhigh, x, left, N)
 
+  # T = knot vector
+  # jhigh = Max number of bases
+  # index = integer that determines the order. jout = max(jhigh, (j+1)*(index-1))
+  #         of the bsplines whose values at x are to be returned. Index is used
+  #         to avoid recalculations when several columns of the triangular Array
+  #         of B-spline values are needed
+  # x = location where bases are evaluated
+  # left = knot span index
+  # N = bases
+  # The calculation starts from scratch and the entire triangular array of B-spline
+  # values of orders 1,2,...,jhigh is generated order by order (column by column)
+
+  jmax = 20 # arbitrarily imposed
+  delta_l = zeros(Float64, jmax)
+  delta_r = zeros(Float64, jmax)
+
+  jout = length(T) - left
+
+  N[1] = 1.0
+
+  for j = 1:(jhigh-1)
+    delta_r[j] = T[left+j] - x
+    delta_l[j] = x - T[left+1-j]
+    saved = 0.0
+    for i = 1:j
+      term = N[i]/(delta_r[i] + delta_l[j+1-i])
+      N[i] = saved + delta_r[i]*term
+      saved = delta_l[j+1-i]*term
+    end  # end for i = 1:r
+    N[j+1] = saved
+  end  # end for j = 1:jhigh
+  # println("N = $N")
+  return nothing
+end
+
+#=
 function basisFunctions(i, u, p, U, N)
 
   # Initialize variables
@@ -34,18 +71,24 @@ function basisFunctions(i, u, p, U, N)
   for j = 1:p
     delta_l[j] = U[i+j] - u
     delta_r[j] = u - U[i+1-j]
+    println("delta_l = $delta_l")
+    println("delta_r = $delta_r")
     saved = 0.0
     for r = 1:j
       term = N[r]/(delta_r[r] + delta_l[j+1-r])
+      println("term = $term")
       N[r] = saved + delta_r[r]*term
       saved = delta_l[j+1-r]*term
+      println("saved = $saved")
+      println("In basis function, N[r] = $(N[r]), N = $N")
     end
     N[j+1] = saved
+    println("\n\n")
   end
   # println("N = $N")
   return nothing
 end  # End function basisFunctions
-
+=#
 @doc """
 ### findSpan
 
@@ -54,7 +97,7 @@ Determines the knot span index, i
 **Inputs**
 
 *  `n` : Number of control points
-*  `p` : Degree of B-spline basis function (Order p+1)
+*  `k` : Order of B-spline basis function
 *  `u` : coordinate value (u,v,w is the coordinate space in 3D)
 *  `U` : Knot vector
 
@@ -66,13 +109,15 @@ SOURCE: The NURBS book 2nd Edition, Algorithm A2.1
 
 """->
 
-function findSpan(u, n, U, p)
+function findSpan(u, n, U, k)
 
   if u == U[n+1]
-    return n  # Special case
+    return n  # Special case when u = last term of knot vector
+  elseif u < U[k]
+    return k  # When u lies at the starting point of the curve
   end
 
-  low = p
+  low = k-1
   high = n+1
   mid = div(low + high, 2)
   # Do a binary search
@@ -117,13 +162,15 @@ function evalCurve(u, U, order, P, C)
   nctl = length(P)
   N = Array(Float64, p+1) # Array of basis functions 1D
   for i = 1:length(u)
-    span = findSpan(u[i], nctl, U, p)
-    println("span = $span, u = $(u[i])")
-    basisFunctions(span, u[i], p, U, N)
+    span = findSpan(u[i], nctl, U, order)
+    println("span = $span, u = $(u[i]), U[span] = $(U[span])")
+    # basisFunctions(span, u[i], p, U, N)
+    basisFunctions(U, order, u[i], span, N)
+    println("N = $N")
     C[i] = 0.0
-    for j = 1:p+1
-      println(span-p+j-2)
-      C[i] += N[j]*P[span-p+j-2] # -2 because compared to algorithm A3.1 this
+    for j = 1:order
+      # println(span - order + j)
+      C[i] += N[j]*P[span-order+j] # -2 because compared to algorithm A3.1 this
                                  # this uses 1 based indexeng. hence (span-1) &
                                  # (j-1) when compared to A3.1
     end  # End for j = 1:p+1
--- test/runtest.jl
+++ test/runtest.jl
@@ -3,21 +3,30 @@ include("../src/b-splines.jl")
 
 # Testing functions based on Ex2.3 from the NURBS book. considering only one
 # value.
-U = [0,0,0,1,2,3,4,4,5,5,5]
+U = [0,0,0,0, 0.1,0.2,0.5,1.0,1.0,1.0,1.0]
+println("U = $U")
 # u = [2.5]
-order = 3
+order = 4
 degree = order - 1
 #n_control_pts = 7 # Figure 2.6 in the NURBS book
 # P = [1,2,3,4,5]
 n_control_pts = length(U) - order
 println("n_control_pts = $n_control_pts")
-P = 0:5/(n_control_pts-1):5
+P = 0:1/(n_control_pts-1):1
 println("P = $P\nlength(P) = ", length(P))
 # Analytical test
-C_test = 0.125*(P[2] + 6*P[3] + P[4])
+C_test = 0.125*(P[3] + 6*P[4] + P[5])
 
-u = [1,2,3,4,5]
-println("val = ", n_control_pts + order - length(U))
+
+u = 0:0.1:1
+# println("val = ", n_control_pts + order - length(U))
+
 C = zeros(Float64,length(u))
+
 evalCurve(u, U, order, P, C)
-println("C = $C")
+@printf("P = [")
+for i = 1:length(P)
+  @printf("%f, ", P[i])
+end
+println("]\nC = $C")
+# println("C analytical = $C_test")
