80401e30efab9cfb5326fe7ab1b72beed4c42c3b
Kinshuk Panda <pandak@rpi.edu>
2016-05-24 15:27:52 -0400

    added documentation

--- src/b-splines.jl
+++ src/b-splines.jl
@@ -2,15 +2,17 @@
 @doc """
 ### basisFunctions
 
-Computes the basis functions needed to compute B-Splines
+Computes the basis functions needed to compute B-Splines. The calculation starts
+from scratch and the entire triangular array of B-spline values of orders
+1,2,...,jhigh is generated order by order (column by column).
 
 **Inputs**
 
-*  `i` : Knot span index
-*  `u` : coordinate value (u,v,w is the coordinate space in 3D)
-*  `p` : Degree of B-spline basis function (Order p+1)
-*  `U` : Knot vector
-*  `N` : Array of basis functions or the B-spline coefficients
+*  `U`     : Knot vector
+*  `order` : Order of B-spline
+*  `u`     : Location where basis functions are to be evaluated
+*  `span`  : Knot span index
+*  `N`     : Bassi Functions
 
 **Outputs**
 
@@ -19,31 +21,17 @@ Computes the basis functions needed to compute B-Splines
 SOURCE: "A practical guide to splines" bt C. de Boor Page 112 BSPLVB
 
 """->
-function basisFunctions(U, order, x, span, N)
-
-  # T = knot vector
-  # jhigh = Max number of bases
-  # index = integer that determines the order. jout = max(jhigh, (j+1)*(index-1))
-  #         of the bsplines whose values at x are to be returned. Index is used
-  #         to avoid recalculations when several columns of the triangular Array
-  #         of B-spline values are needed
-  # x = location where bases are evaluated
-  # span = knot span index
-  # N = bases
-  # The calculation starts from scratch and the entire triangular array of B-spline
-  # values of orders 1,2,...,jhigh is generated order by order (column by column)
+function basisFunctions(U, order, u, span, N)
 
   jmax = 20 # arbitrarily imposed
   delta_l = zeros(Float64, jmax)
   delta_r = zeros(Float64, jmax)
 
-  # jout = length(U) - span
-
   N[1] = 1.0
 
   for j = 1:(order-1)
-    delta_r[j] = U[span+j] - x
-    delta_l[j] = x - U[span+1-j]
+    delta_r[j] = U[span+j] - u
+    delta_l[j] = u - U[span+1-j]
     saved = 0.0
     for i = 1:j
       term = N[i]/(delta_r[i] + delta_l[j+1-i])
@@ -51,44 +39,11 @@ function basisFunctions(U, order, x, span, N)
       saved = delta_l[j+1-i]*term
     end  # end for i = 1:r
     N[j+1] = saved
-  end  # end for j = 1:jhigh
-  # println("N = $N")
+  end  # end for j = 1:(order-1)
+
   return nothing
 end
 
-#=
-function basisFunctions(i, u, p, U, N)
-
-  # Initialize variables
-  jmax = 20
-  delta_l = Array(Float64, jmax)
-  delta_r = Array(Float64, jmax)
-  saved = 0.0
-  # jhigh = p+1
-  # println("jhigh = $jhigh")
-
-  N[1] = 1.0
-  for j = 1:p
-    delta_l[j] = U[i+j] - u
-    delta_r[j] = u - U[i+1-j]
-    println("delta_l = $delta_l")
-    println("delta_r = $delta_r")
-    saved = 0.0
-    for r = 1:j
-      term = N[r]/(delta_r[r] + delta_l[j+1-r])
-      println("term = $term")
-      N[r] = saved + delta_r[r]*term
-      saved = delta_l[j+1-r]*term
-      println("saved = $saved")
-      println("In basis function, N[r] = $(N[r]), N = $N")
-    end
-    N[j+1] = saved
-    println("\n\n")
-  end
-  # println("N = $N")
-  return nothing
-end  # End function basisFunctions
-=#
 @doc """
 ### findSpan
 
