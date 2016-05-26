096bb1f16fe296f31dee935adfc3df0aea79bd7d
Kinshuk Panda <pandak@rpi.edu>
2016-05-25 21:08:19 -0400

    First attempt at coding derivative value

--- src/b-splines.jl
+++ src/b-splines.jl
@@ -44,6 +44,97 @@ function basisFunctions(U, order, u, span, N)
   return nothing
 end
 
+function derivValue(U, order, u, span, P, Nderiv, nctl, jth_deriv)
+
+  @assert length(U) == order + nctl
+  # Number of control points = number of basis functions
+
+  bvalue = 0
+
+  if jth_deriv > order
+    return bvalue
+  end
+
+  if k == 1
+    bvalue = P[span]
+    return bvalue
+  else
+    # Store b-spline coefficients relevant to the knot interval (U[span], U[span+1])
+    # in another vector aj[1:order] and compute dl[j] = u -U[span+1-j],
+    # dr[j] = U[span+j] - u, j = 1,...,order-1 . Set any of the aj not obtainable
+    # from input to zero. Set any U.s not obtainable to U[1] or to U[length(P)+order]
+    # appropriately
+    jcmin = 1
+    imk = span - order
+    if imk > 0
+      for j = 1:(order-1)
+        dl[j] = u - U[span+1-j]
+      end  # end for j = 1:k-1
+    else
+      jcmin = 1 - imk
+      for j = 1:span
+        dl[j] = u - U[span+1-j]
+      end  # end for j = 1:span
+
+      for j = span:(order-1)
+        aj[order-j] = 0.0
+        dl[j] = dl[span]
+      end
+    end  # end if imk > 0
+
+    jcmax = order
+    nmi = length(P) - span
+    if nmi > 0
+      for j = 1:(order-1)
+        dr[j] = U[span+j] - u
+      end
+    else
+      jcmax = order + nmi
+      for j = 1:jcmax
+        dr[j] = U[span+j] - u
+      end
+
+      for j = jcmax:(order-1)
+        aj[j+1] = 0
+        dr[j] = dr[jcmax]
+      end
+    end  # end if-else nmi > 0
+
+    for jc = jcmin:jcmax
+      aj[jc] = P[imk+jc]
+    end
+
+    # difference the coefficients jth_deriv times
+    if jth_deriv == 0
+      # compute value at u in (U[span], U[span+1]) of jth_deriv derivative,
+      # given its relevant b-spline coefficients/control points in
+      # aj[1],...,aj[order-jth_deriv]
+      if jth_deriv == order - 1
+        bvalue = aj[1]
+      else
+        for j = (jth_deriv+1):(order-1)
+          ilo = order - j
+          for jj = 1:(order-j)
+            aj[jj] = ( aj[jj+1]*dl[ilo] + ajj[jj]*dr[jj] )/( dl[ilo] + dr[jj] )
+            ilo -= 1
+          end
+        end
+      end
+
+    else
+      for j = 1:jth_deriv
+        ilo = order - j
+        for jj = 1:(order-j)
+          aj[jj] = ( (aj[jj+1] - aj[jj]) / (dl[ilo] + dr[jj]) ) * (order - j)
+          ilo -= 1
+        end
+      end
+
+  end # End if-else statement for k == 1
+
+  return bvalue
+end
+
 @doc """
 ### findSpan
 
@@ -97,7 +188,7 @@ Determine the value of curve at certain points
 
 *  `u` : Array of coordinates where the curve needs to be computed
 *  `U` : Knot vector
-*  `order` : order of B-spline basis functio, (order = p+1)
+*  `order` : order of B-spline basis functions, (order = p+1)
 *  `P` : Array of control points
 *  `C` : Resulting curve values
 
