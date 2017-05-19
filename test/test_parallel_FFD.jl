ca9bbe924453c1af5df46751733b832287912477
Kinshuk Panda <pandak@rpi.edu>
2017-05-19 15:07:25 -0400

    added files for parallel tests, all the tests in test_parallel_FFD.jl pass as of this commit

---
+++ test/test_parallel_FFD.jl
@@ -0,0 +1,145 @@
+# Test FFD function that make parallel MPI calls
+
+
+# MPI Declarations
+if !MPI.Initialized()
+  MPI.Init()
+end
+comm = MPI.COMM_WORLD
+comm_world = MPI.MPI_COMM_WORLD
+comm_self = MPI.COMM_SELF
+my_rank = MPI.Comm_rank(comm)
+comm_size = MPI.Comm_size(comm)
+
+opts = PdePumiInterface.get_defaults()
+# 2D mesh
+opts["order"] = 1
+opts["dimensions"] = 2
+opts["use_DG"] = true
+opts["operator_type"] = "SBPOmega"
+opts["dmg_name"] = ".null"
+opts["smb_name"] = "../src/mesh_files/gvortex1np2.smb"
+opts["numBC"] = 2
+
+# For 2DAirfoil
+opts["numBC"] = 4
+opts["BC1"] = [0]
+opts["BC1_name"] = "FreeStreamBC"
+opts["BC2"] = [1]
+opts["BC2_name"] = "FreeStreamBC"
+opts["BC3"] = [2]
+opts["BC3_name"] = "FreeStreamBC"
+opts["BC4"] = [3]
+opts["BC4_name"] = "noPenetrationBC"
+
+opts["coloring_distance"] = 2 # 0 For CG Mesh 2 for DG Mesh
+opts["jac_type"] = 1
+opts["run_type"] = 1
+opts["jac_method"] = 2
+opts["use_jac_precond"] = true
+
+# Create PumiMesh and SBP objects
+sbp, mesh, pmesh, Tsol, Tres, Tmsh, mesh_time = createMeshAndOperator(opts, 1)
+
+facts("--- Checking control point manipulation for 2D parallel mesh") do
+
+  context("Check for the entire mesh") do
+    ndim = mesh.dim
+    order = [4,4,2]  # Order of B-splines in the 3 directions
+    nControlPts = [4,4,2]
+    offset = [0., 0., 0.5] # No offset in the X & Y direction
+
+    # Create a mapping object using nonlinear mapping
+    map = initializeFFD(mesh, sbp, order, nControlPts, offset, true)
+
+    # Control Point Manipulation
+    #  - Rigid body rotation
+    theta = -90*pi/180  # Rotate wall coordinates by 90 degrees
+    rotMat = [cos(theta) -sin(theta) 0
+              sin(theta) cos(theta)  0
+              0          0           1] # Rotation matrix
+    # - Rotate the control points
+    for k = 1:map.nctl[3]
+      for j = 1:map.nctl[2]
+        for i = 1:map.nctl[1]
+          map.cp_xyz[:,i,j,k] = rotMat*map.cp_xyz[:,i,j,k]
+        end
+      end
+    end
+
+    # - Rigid body translation
+    map.cp_xyz[1,:,:,:] += 0.2
+    map.cp_xyz[2,:,:,:] += 0.3
+
+    vertices = evalVolume(map, mesh)
+    commitToPumi(map, mesh, sbp, vertices)
+
+    filename = string("translation_plus_rotation_DG_parallel_fullmesh_", mesh.myrank, ".dat")
+    # f = open(string("./testvalues/", filename), "w")
+    # for i = 1:length(vertices)
+    #   println(f, vertices[i])
+    # end
+    # close(f)
+
+    test_vtx_coords = readdlm(string("./testvalues/", filename))
+    for i = 1:length(test_vtx_coords)
+      err = abs(test_vtx_coords[i] - mesh.vert_coords[i])
+      @fact err --> less_than(1e-14)
+    end
+
+    # writeVisFiles(mesh, "translation_plus_rotation_DG_parallel_fullmesh")
+
+  end
+
+  context("Check for a geometric surface") do
+
+    # Free Form deformation parameters
+    ndim = mesh.dim
+    order = [4,4,2]  # Order of B-splines in the 3 directions
+    nControlPts = [4,4,2]
+    offset = [0., 0., 0.5] # No offset in the X & Y direction
+    Tmsh = Float64
+    geom_faces = opts["BC4"]
+
+    # Create a mapping object using nonlinear mapping
+    map = initializeFFD(mesh, sbp, order, nControlPts, offset, false, geom_faces)
+
+    # Control Point Manipulation
+    #  - Rigid body rotation
+    theta = -5*pi/180  # Rotate wall coordinates by 5 degrees
+    rotMat = [cos(theta) -sin(theta) 0
+              sin(theta) cos(theta)  0
+              0          0           1] # Rotation matrix
+    # - Rotate the control points
+    for k = 1:map.nctl[3]
+      for j = 1:map.nctl[2]
+        for i = 1:map.nctl[1]
+          map.cp_xyz[:,i,j,k] = rotMat*map.cp_xyz[:,i,j,k]
+        end
+      end
+    end
+
+    # - Rigid body translation
+    map.cp_xyz[1,:,:,:] += 0.05
+
+    vertices = evalSurface(map, mesh)
+    commitToPumi(map, mesh, sbp, vertices)
+
+    filename = string("translation_plus_rotation_DG_parallel_surfaceBC4_", mesh.myrank, ".dat")
+    # f = open(string("./testvalues/", filename), "w")
+    # for i = 1:length(mesh.vert_coords)
+    #  println(f, mesh.vert_coords[i])
+    # end
+    # close(f)
+
+    test_vtx_coords = readdlm(string("./testvalues/", filename))
+    for i = 1:length(test_vtx_coords)
+      err = abs(test_vtx_coords[i] - mesh.vert_coords[i])
+      @fact err --> less_than(1e-14)
+    end
+
+    # writeVisFiles(mesh, "translation_plus_rotation_DG_parallel_surface")
+
+  end # End context("Check for a geometric surface")
+
+end # End facts("--- Checking control point manipulation for 2D parallel mesh")
