9e81e1eead3517154dc62b295c32a924231466a4
Jared Crean <jcrean01@gmail.com>
2018-05-29 13:19:57 -0400

    start upgrade to julia 0.5

--- deps/build.jl
+++ deps/build.jl
@@ -2,15 +2,16 @@
 if !isdir(joinpath(Pkg.dir(), "PkgFix"))
   Pkg.clone("https://github.com/OptimalDesignLab/PkgFix.jl.git")
 end
+Pkg.checkout("PkgFix", "upgrade_0.5")
 
 using PkgFix  # from now on, use PkgFix instead of Pkg for everything
 
-pkgs = ["https://github.com/OptimalDesignLab/PumiInterface.jl.git" "curvilinear";
+pkgs = ["https://github.com/OptimalDesignLab/PumiInterface.jl.git" "upgrade_0.5";
         "https://github.com/JuliaParallel/MPI.jl.git"  "v0.5.0";
-        "https://github.com/OptimalDesignLab/SummationByParts.jl.git"  "jcwork";
-        "https://github.com/JuliaLang/ArrayViews.jl.git" "93e80390aeedb1dbcd90281b6dff7f760f430bc8";
-        "https://github.com/jipolanco/WriteVTK.jl.git"  "v0.6.1";
-        "https://github.com/OptimalDesignLab/ODLCommonTools.jl.git" "new_parallel"]
+        "https://github.com/OptimalDesignLab/SummationByParts.jl.git"  "jc_update_0.5";
+#        "https://github.com/JuliaLang/ArrayViews.jl.git" "93e80390aeedb1dbcd90281b6dff7f760f430bc8";
+#        "https://github.com/jipolanco/WriteVTK.jl.git"  "v0.6.1";
+        "https://github.com/OptimalDesignLab/ODLCommonTools.jl.git" "upgrade_0.5"]
 
 
 
