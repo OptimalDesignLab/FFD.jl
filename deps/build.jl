ab1da810a396d50365885d2a55a3131b05b715c0
Jared Crean <jcrean01@gmail.com>
2018-06-05 14:33:52 -0400

    change ArrayViews version

--- deps/build.jl
+++ deps/build.jl
@@ -10,12 +10,12 @@ cd(start_dir)
 
 using PkgFix  # from now on, use PkgFix instead of Pkg for everything
 
-pkgs = ["https://github.com/OptimalDesignLab/PumiInterface.jl.git" "update_0.6";
+pkgs = ["https://github.com/OptimalDesignLab/PumiInterface.jl.git" "v0.8";
 #        "https://github.com/JuliaParallel/MPI.jl.git"  "v0.5.0";
-        "https://github.com/OptimalDesignLab/SummationByParts.jl.git"  "jc_update_0.6";
+        "https://github.com/OptimalDesignLab/SummationByParts.jl.git"  "jc_v0.3";
 #        "https://github.com/JuliaLang/ArrayViews.jl.git" "93e80390aeedb1dbcd90281b6dff7f760f430bc8";
 #        "https://github.com/jipolanco/WriteVTK.jl.git"  "v0.6.1";
-        "https://github.com/OptimalDesignLab/ODLCommonTools.jl.git" "upgrade_0.6"]
+        "https://github.com/OptimalDesignLab/ODLCommonTools.jl.git" "v0.4"]
 
 
 
@@ -28,8 +28,9 @@ for i=1:size(pkgs, 1)
   end
 end
 
-global const ARRAYVIEWS_URL = "http://github.com/JuliaArrays/ArrayViews.jl.git"
-global const ARRAYVIEWS_VER = "master"
+global const ARRAYVIEWS_URL = "https://github.com/JaredCrean2/ArrayViews.jl.git"
+global const ARRAYVIEWS_VER = "work"
+
 
 if !haskey(pkg_dict, "ArrayViews")
   PkgFix.add(ARRYAVIEWS_URL, branch_ish=ARRAYVIEWS_VER)
